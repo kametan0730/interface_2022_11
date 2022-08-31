90ae180bf1cc9be815bdbd0fb46d01d76aa22dbe
kametan0730 <kametan0730@gmail.com>
2022-08-31 18:06:29 +0900

    some changes

--- chapter4-netns.sh
+++ chapter4-netns.sh
@@ -3,11 +3,13 @@
 # rootユーザーが必要
 if [ $UID -ne 0 ]; then
   echo "Root privileges are required"
-  exit 1;
+  exit 1
 fi
 
 function setup_link() {
-  ip netns exec $1 ip addr add $3 dev $2
+  if [ $# -ge 3 ]; then
+    ip netns exec $1 ip addr add $3 dev $2
+  fi
   ip netns exec $1 ip link set $2 up
   ip netns exec $1 ethtool -K $2 rx off tx off
 }
@@ -24,12 +26,12 @@ ip netns add router4
 ip netns add host2
 
 # リンクの作成
-ip link add name host1-router1 type veth peer name router1-host1 # host1とrouter1のリンク
+ip link add name host1-router1 type veth peer name router1-host1     # host1とrouter1のリンク
 ip link add name router1-router2 type veth peer name router2-router1 # router1とrouter2のリンク
 ip link add name router2-router3 type veth peer name router3-router2 # router2とrouter3のリンク
 ip link add name router3-router4 type veth peer name router4-router3 # router3とrouter4のリンク
 ip link add name router1-router4 type veth peer name router4-router1 # router1とrouter4のリンク
-ip link add name router3-host2 type veth peer name host2-router3 # router3とhost2のリンク
+ip link add name router3-host2 type veth peer name host2-router3     # router3とhost2のリンク
 
 # リンクの割り当て
 ip link set host1-router1 netns host1
@@ -50,12 +52,9 @@ setup_link host1 host1-router1 192.168.0.2/24
 ip netns exec host1 ip route add default via 192.168.0.1
 
 # router1のリンクの設定
-ip netns exec router1 ip link set router1-host1 up
-ip netns exec router1 ethtool -K router1-host1 rx off tx off
-ip netns exec router1 ip link set router1-router2 up
-ip netns exec router1 ethtool -K router1-router2 rx off tx off
-ip netns exec router1 ip link set router1-router4 up
-ip netns exec router1 ethtool -K router1-router4 rx off tx off
+setup_link router1 router1-host1
+setup_link router1 router1-router2
+setup_link router1 router1-router4
 
 # router2のリンクの設定
 setup_link router2 router2-router1 192.168.1.2/24
@@ -68,7 +67,7 @@ ip netns exec router2 sysctl -w net.ipv4.ip_forward=1
 setup_link router3 router3-router2 192.168.2.2/24
 setup_link router3 router3-router4 192.168.3.1/24
 setup_link router3 router3-host2 192.168.5.1/24
-ip netns exec router3 ip route add 192.168.0.0/24 via 192.168.2.1
+ip netns exec router3 ip route add 192.168.0.0/24 via 192.168.3.2
 ip netns exec router3 sysctl -w net.ipv4.ip_forward=1
 
 # router4のリンクの設定
@@ -81,5 +80,3 @@ ip netns exec router4 sysctl -w net.ipv4.ip_forward=1
 # host2のリンクの設定
 setup_link host2 host2-router3 192.168.5.2/24
 ip netns exec host2 ip route add default via 192.168.5.1
-
-
