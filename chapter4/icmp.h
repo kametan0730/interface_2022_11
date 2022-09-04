f14286818efb1a812fa389cc05a60219480867bc
kametan0730 <kametan0730@gmail.com>
2022-09-04 19:17:43 +0900

    ICMPの変更

--- chapter4/icmp.h
+++ chapter4/icmp.h
@@ -15,20 +15,33 @@ struct icmp_header{
 } __attribute__((packed));
 
 struct icmp_echo{
-    icmp_header header;
     uint16_t identify;
     uint16_t sequence;
     uint8_t data[];
 } __attribute__((packed));
 
-#define ICMP_TIME_EXCEEDED_CODE_TIME_TO_LIVE_EXCEEDED 0
-#define ICMP_TIME_EXCEEDED_CODE_FRAGMENT_REASSEMBLY_TIME_EXCEEDED 1
-
-struct icmp_time_exceeded{
-    icmp_header header;
+struct icmp_destination_unreachable{
     uint32_t unused;
     uint8_t data[];
 } __attribute__((packed));
+
+struct icmp_time_exceeded{
+    uint32_t unused;
+    uint8_t data[];
+} __attribute__((packed));
+
+struct icmp_message{
+    icmp_header header;
+    union{
+        icmp_echo echo;
+        icmp_destination_unreachable destination_unreachable;
+        icmp_time_exceeded time_exceeded;
+    };
+} __attribute__((packed));
+
+#define ICMP_TIME_EXCEEDED_CODE_TIME_TO_LIVE_EXCEEDED 0
+#define ICMP_TIME_EXCEEDED_CODE_FRAGMENT_REASSEMBLY_TIME_EXCEEDED 1
+
 
 void icmp_input(uint32_t source, uint32_t destination, void *buffer, size_t len);
 
